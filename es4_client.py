f5f938b1c2f2f933839165f63f1437e7a46f5e7e
wdpen <54875044+wdpen@users.noreply.github.com>
2019-09-15 05:36:46 -0400

    Add files via upload

---
+++ es4_client.py
@@ -0,0 +1,58 @@
+import socket, asyncio
+import random, sys, time
+
+
+class EchoClient(asyncio.Protocol):	
+	def __init__(self):
+		self.escapestep=['look mirror<EOL>\n','get hairpin<EOL>\n','unlock chest with hairpin<EOL>\n',
+							'open chest<EOL>\n','get hammer in chest<EOL>\n','hit flyingkey with hammer<EOL>\n',
+							'get key<EOL>\n','unlock door with key<EOL>\n','open door<EOL>\n']
+		self.es_iter=0;
+		self.es_itst=5;
+		self.fla=0;
+
+	def connection_made(self, transport):
+		self.transport = transport
+
+	def data_received(self, data):
+		print(data)
+		data=data.decode()
+		data=data.split('<EOL>\n')
+		if self.fla==0:
+			for line in data:
+				if line!='':
+					seli=line.split(' ')
+					if 'autograde' in seli:
+						self.transport.write("SUBMIT,Haoshuai Ding,dhaoshu1@jhu.edu,7,1810<EOL>\n".encode())
+						return
+					if ('SUBMIT' in seli) and ('OK' in seli):
+						self.fla=1;
+						return
+		if self.es_iter<len(self.escapestep):
+			if self.es_iter!=self.es_itst:
+					self.transport.write(self.escapestep[self.es_iter].encode())
+					self.es_iter+=1
+			else:
+				for line in data:
+					if line!='':
+						seli=line.split(' ')
+						if (seli[-1]=='wall'):
+							self.transport.write(self.escapestep[self.es_iter].encode())						
+							self.es_iter+=1
+							break
+
+
+if __name__ == "__main__":
+	loop = asyncio.get_event_loop()
+	coro = loop.create_connection(EchoClient,'localhost',2345)
+	client = loop.run_until_complete(coro)
+
+	try:
+		loop.run_forever()
+	except KeyboardInterrupt:
+		loop.close()
+	#client.close()
+	#loop.run_until_complete(client.close())
+	#loop.run_forever()
+	
+	loop.close()
