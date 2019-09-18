f87c94ae4b2b812a3239e5277f0280b38d17aeda
wdpen <54875044+wdpen@users.noreply.github.com>
2019-09-18 03:10:48 -0400

    Add files via upload

--- es5_client.py
+++ es5_client.py
@@ -4,16 +4,43 @@ import playground
 
 class EchoClient(asyncio.Protocol):	
 	def __init__(self):
-		pass
+		self.escapestep=['look mirror<EOL>\n','get hairpin<EOL>\n','unlock chest with hairpin<EOL>\n',
+							'open chest<EOL>\n','get hammer in chest<EOL>\n','hit flyingkey with hammer<EOL>\n',
+							'get key<EOL>\n','unlock door with key<EOL>\n','open door<EOL>\n']
+		self.es_iter=0;
+		self.es_itst=5;
+		self.fla=0;
 
 	def connection_made(self, transport):
-		print('connection_made')
 		self.transport = transport
-		self.transport.write(b'<EOL>\n')
+		self.transport.write('<EOL>\n'.encode())
 
 	def data_received(self, data):
 		print(data)
-		self.transport.write(b'sending')
+		data=data.decode()
+		data=data.split('<EOL>\n')
+		if self.fla==0:
+			for line in data:
+				if line!='':
+					seli=line.split(' ')
+					if 'autograde' in seli:
+						self.transport.write("SUBMIT,Haoshuai Ding,dhaoshu1@jhu.edu,7,1810<EOL>\n".encode())
+						return
+					if ('SUBMIT' in seli) and ('OK' in seli):
+						self.fla=1;
+						return
+		if self.es_iter<len(self.escapestep):
+			if self.es_iter!=self.es_itst:
+					self.transport.write(self.escapestep[self.es_iter].encode())
+					self.es_iter+=1
+			else:
+				for line in data:
+					if line!='':
+						seli=line.split(' ')
+						if (seli[-1]=='wall'):
+							self.transport.write(self.escapestep[self.es_iter].encode())						
+							self.es_iter+=1
+							break
 
 
 if __name__ == "__main__":
