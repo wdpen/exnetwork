bdc84c4d5898d145dc5341f4d6d988622214f763
wdpen <54875044+wdpen@users.noreply.github.com>
2019-09-18 03:17:01 -0400

    Add files via upload

---
+++ escheckgrade.py
@@ -0,0 +1,36 @@
+import socket, asyncio
+import random, sys, time
+import playground
+
+class EchoClient(asyncio.Protocol):	
+	def __init__(self):
+
+	def connection_made(self, transport):
+		self.transport = transport
+		self.transport.write('<EOL>\n'.encode())
+
+	def data_received(self, data):
+		print(data)
+		data=data.decode()
+		data=data.split('<EOL>\n')
+		for line in data:
+			if line!='':
+				seli=line.split(' ')
+				if 'autograde' in seli:
+					self.transport.write("RESULT,ecfc531906978e1f2ad19b9aadfb728f3c8de39cd9c0a4f585363b2610ba33cd<EOL>\n".encode())
+					return
+
+if __name__ == "__main__":
+	loop = asyncio.get_event_loop()
+	coro = playground.create_connection(EchoClient,'20194.0.0.19000',19005)
+	client = loop.run_until_complete(coro)
+
+	try:
+		loop.run_forever()
+	except KeyboardInterrupt:
+		loop.close()
+	#client.close()
+	#loop.run_until_complete(client.close())
+	#loop.run_forever()
+	
+	loop.close()
