962be24ea568c7c12c5599a7c3d9c0b3bda8ffce
wdpen <54875044+wdpen@users.noreply.github.com>
2019-09-29 06:20:50 -0400

    Add files via upload

--- es7.py
+++ es7.py
@@ -150,10 +150,9 @@ class EchoServer(asyncio.Protocol):
 				#password = getpass.getpass("Enter password for {}: ".format(self.gameholder))
 				password='dpo%symp8h!onic'
 				bank_client = BankClientProtocol(bank_cert, self.gameholder, password)
-				asyncio.ensure_future(playground.create_connection(lambda: bank_client,bank_addr,bank_port,family='default'))
-				asyncio.ensure_future(bank_client.loginToServer())
+				loop.run_until_complete(playground.create_connection(lambda: bank_client,bank_addr,bank_port,family='default'))
+				loop.run_until_complete(bank_client.loginToServer())
 				#asyncio.ensure_future(example_verify(bank_client, recvpack.receipt, recvpack.receipt_signature, self.account, self.amount, self.unique_id))
-				loop.run_until_complete()
 				if (example_verify(bank_client, recvpack.receipt, recvpack.receipt_signature, self.account, self.amount, self.unique_id)):
 					print('Server verified the payment, sent starting game response.')
 					self.game.create_game()
