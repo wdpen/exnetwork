a2b7deae262163c2b254dc93b97e0aa3d5ed37e0
wdpen <54875044+wdpen@users.noreply.github.com>
2019-09-29 05:53:47 -0400

    Add files via upload

--- es7.py
+++ es7.py
@@ -211,7 +211,7 @@ class EchoClient(asyncio.Protocol):
 				#password = getpass.getpass("Enter password for {}: ".format(self.username))
 				password='dpo%symp8h!onic'
 				bank_client = BankClientProtocol(bank_cert, self.username, password) 
-				result=loop.run_until_complete(example_transfer(bank_client, self.useraccount, recvpack.account, recvpack.amount, recvpack.unique_id))
+				result=loop.run_until_complete(example_transfer(bank_client, self.useraccount, recvpack.account, recvpack.amount, recvpack.unique_id, self.transport))
 				#asyncio.ensure_future(example_transfer(bank_client, self.useraccount, recvpack.account, recvpack.amount, recvpack.unique_id, self.transport))
 				if result:
 					print('BBBBBBBBBBBBBB.')
